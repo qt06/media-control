c99254b56ab3f8559009fffb3b4569891f932503
qt06 <qt06.com@139.com>
2020-10-08 15:59:27 +0800

    first commit

---
+++ dist/media-control.js
@@ -0,0 +1,64 @@
+(function () {
+	'use strict';
+
+	function getMedia() {
+	return document.querySelector('audio, video');
+	}
+	function setMediaVolume(op) {
+	let c = getMedia();
+	if(c == null) { return false; }
+	if(op == '+') {
+	c.volume = Math.min(c.volume + 0.05, 1);
+	} else {
+	c.volume = Math.max(c.volume - 0.05, 0);
+	}
+	}
+	function setMediaCurrentTime(op) {
+	let c = getMedia();
+	if(c == null) { return false; }
+	if(op == '+') {
+	c.currentTime += 5;
+	} else {
+	c.currentTime -= 5;
+	}
+	}
+	function toggleMediaPlay() {
+	let c = getMedia();
+	if(c == null) { return false; }
+	if(c.paused) {
+	c.play();
+	} else {
+	c.pause();
+	}
+	}
+	let c = getMedia();
+	if(c != null) {
+	document.addEventListener('keydown', function(e) {
+	let keyCode = e.keyCode;
+	let shiftKey = e.shiftKey;
+	if(keyCode == 32 || keyCode == 27) {
+	e.preventDefault();
+	toggleMediaPlay();
+	}
+	if(shiftKey && keyCode == 38) {
+	e.preventDefault();
+	setMediaVolume('+');
+	}
+	if(shiftKey && keyCode == 40) {
+	e.preventDefault();
+	setMediaVolume('-');
+	}
+	if(shiftKey && keyCode == 37) {
+	e.preventDefault();
+	setMediaCurrentTime('-');
+	}
+	if(shiftKey && keyCode == 39) {
+	e.preventDefault();
+	setMediaCurrentTime('+');
+	}
+
+	}, null);
+	}
+
+}());
+//# sourceMappingURL=media-control.js.map
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import minimist from 'minimist';
+import resolve from '@rollup/plugin-node-resolve';
+import json from 'rollup-plugin-json';
+import { terser } from 'rollup-plugin-terser';
+
+let argv = minimist(process.argv.slice(2));
+let minify = argv['config:minify'];
+const build = (filename) => {
+return {
+  input: 'src/' + filename + '.js',
+  output: {
+    file: minify ? 'dist/' + filename + '.min.js' : 'dist/' + filename + '.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+    json(),
+    minify ? terser() : {}
+  ]
+}};
+export default [
+build('media-control'),
+];
